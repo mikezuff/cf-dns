fb94cec5eac33b1535aea58251d763cf09d15856
Michael Zuffoletti <mikezuff@gmail.com>
2021-11-05 10:00:35 -0700

    test how record IDs work: they don't change but do disappear on deletion
    so nothing magic, just a fixed identifier

--- main.go
+++ main.go
@@ -4,37 +4,49 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
 	"net/http/httptrace"
 	"os"
+	"strings"
 
 	"github.com/cloudflare/cloudflare-go"
 )
 
+func usage() {
+	log.Printf("Usage: %s [options] -zone <zone_name>\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
 func main() {
 	var (
-		trace bool
+		trace    bool
+		testZone string
 	)
 	flag.BoolVar(&trace, "trace", false, "enable HTTP tracing")
+	flag.StringVar(&testZone, "zone", "", "test zone")
 	flag.Parse()
 
-	api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
+	if testZone == "" {
+		usage()
+		log.Fatal("missing zone")
+	}
+
+	api, err := cloudflare.New(os.Getenv("CLOUDFLARE_API_KEY"), os.Getenv("CLOUDFLARE_API_EMAIL"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("error creating API:", err)
 	}
 
 	ctx := context.Background()
 	if trace {
 		ht := &httptrace.ClientTrace{
 			GotConn: func(connInfo httptrace.GotConnInfo) {
-				fmt.Printf("Got Conn: %+v\n", connInfo)
+				log.Printf("Got Conn: %+v\n", connInfo)
 			},
 			DNSDone: func(dnsInfo httptrace.DNSDoneInfo) {
-				fmt.Printf("DNS Info: %+v\n", dnsInfo)
+				log.Printf("DNS Info: %+v\n", dnsInfo)
 			},
 			WroteHeaderField: func(key string, value []string) {
-				fmt.Printf("Wrote Header: %s: %v\n", key, value)
+				log.Printf("Wrote Header: %s: %v\n", key, value)
 			},
 		}
 		ctx = httptrace.WithClientTrace(ctx, ht)
@@ -42,28 +54,97 @@ func main() {
 
 	u, err := api.UserDetails(ctx)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("error: UserDetails:", err)
 	}
 
-	id, err := api.ZoneIDByName("zuff.dev")
+	zid, err := api.ZoneIDByName(testZone)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("error: ZoneIDByName:", err)
 	}
 
-	zone, err := api.ZoneDetails(ctx, id)
+	zone, err := api.ZoneDetails(ctx, zid)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("error: ZoneDetails:", err)
+	}
+
+	recs, err := api.DNSRecords(ctx, zid, cloudflare.DNSRecord{})
+	if err != nil {
+		log.Fatal("error: DNSRecords:", err)
+	}
+
+	testNames := []string{"cf-dns-test-1", "cf-dns-test-2"}
+	found := false
+	for _, testName := range testNames {
+		for _, rec := range recs {
+			if strings.HasPrefix(rec.Name, testName) {
+				found = true
+				break
+			}
+		}
+	}
+
+	var (
+		rrIDs       []string // DNSRecord IDs for test names
+		respRecords []*cloudflare.DNSRecord
+	)
+	if found {
+		log.Printf("Zone %s contains one of the test names %v, not modifying the zone.\n",
+			testZone, testNames)
+	} else {
+		resp1, err := api.CreateDNSRecord(ctx, zid, cloudflare.DNSRecord{
+			Type:    "CNAME",
+			Name:    testNames[0],
+			Content: "ns1.zuffs.net",
+			TTL:     1,
+		})
+		if err != nil {
+			log.Fatal("error: CreateDNSRecord", err)
+		}
+		if !resp1.Success {
+			log.Fatalf("creation of %s failed: %v\n", testNames[0], resp1.Errors)
+		}
+		rrIDs = append(rrIDs, resp1.Result.ID)
+		respRecords = append(respRecords, &resp1.Result)
+
+		testRR2 := resp1.Result
+		testRR2.Name = testNames[1]
+		err = api.UpdateDNSRecord(ctx, zid, rrIDs[0], testRR2)
+		if err != nil {
+			log.Fatal("update failed", err)
+		}
+		rec2, err := api.DNSRecord(ctx, zid, rrIDs[0])
+		if err != nil {
+			log.Fatal("get2 failed:", err)
+		}
+		rrIDs = append(rrIDs, rec2.ID)
+		respRecords = append(respRecords, &rec2)
+
+		err = api.DeleteDNSRecord(ctx, zid, rrIDs[1])
+		if err != nil {
+			log.Fatal("deletion failed", err)
+		}
+
+		rec3, err := api.DNSRecord(ctx, zid, rrIDs[0])
+		if err != nil {
+			log.Printf("Get after Delete returned error: %s", err)
+		} else {
+			log.Printf("After deletion, query for %s returned a result\n", rrIDs[0])
+			respRecords = append(respRecords, &rec3)
+		}
 	}
 
 	enc := json.NewEncoder(os.Stdout)
-
 	type out struct {
-		User cloudflare.User `json:"user"`
-		Zone cloudflare.Zone `json:"zone"`
+		User      cloudflare.User         `json:"user"`
+		Zone      cloudflare.Zone         `json:"zone"`
+		IDs       []string                `json:"ids"`
+		Responses []*cloudflare.DNSRecord `json:"responses"`
 	}
 	o := out{
-		User: u,
-		Zone: zone,
+		User:      u,
+		Zone:      zone,
+		IDs:       rrIDs,
+		Responses: respRecords,
 	}
 	enc.Encode(o)
 }
